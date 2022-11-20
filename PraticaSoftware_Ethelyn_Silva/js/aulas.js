bca630a2b98d0120d56a08579bdcc4248cb246c4
Ethelynaparecida <ethelynaparecida@gmail.com>
2022-11-20 20:25:22 -0300

    Atividade Prática 14

--- PraticaSoftware_Ethelyn_Silva/js/aulas.js
+++ PraticaSoftware_Ethelyn_Silva/js/aulas.js
@@ -9,3 +9,22 @@ function carregar(){
 
     console.log(nome.value + " " + sobrenome.value + " "+ email.value);
 }
+function carregarNotas(){
+    var nota1 = document.getElementById("nota1");
+    var nota2 = document.getElementById("nota2");
+    var soma = parseInt(nota1.value) + parseInt(nota2.value);
+
+    if (soma > 10){
+        jQuery('#informacoesNotas').html("Valor acima de 10");
+        $('#informacoesNotas').css('background-color', 'green');
+        $('#informacoesNotas').css('color', 'white');
+    }else{
+        jQuery('#informacoesNotas').html("Valor abaixo de 10");
+        $('#informacoesNotas').css('background-color', '#cc4c4c');
+        $('#informacoesNotas').css('color', 'white');
+    }
+
+  
+
+    console.log("nota = "+soma);
+}
